7c898a78a914ccba41b1c4c690a22fbacef437cf
Deepankar Saxena <deepankarsaxena1403@gmail.com>
2018-11-26 22:13:45 +0530

    Added connection with DB

--- application.py
+++ application.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, BookCategory, Book
+
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///bookstore.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+@app.route('/bookCategory/<int:bookCategory_id>/')
+def listofbooks(bookCategory_id):
+    bookCategory = session.query(BookCategory).filter_by(id = bookCategory_id).one()
+    books = session.query(Book).filter_by(bookCategory_id=bookCategory_id)
+    output = ''
+    for i in books:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'       
+    return output
 
 if __name__ == '__main__':
     app.debug = True
